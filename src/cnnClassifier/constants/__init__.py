5b28ca18b850750ac6bbcfaaec30fdb26eb3afcb
itsinaam <itsinaam89@gmail.com>
2024-01-01 15:25:33 +0500

    1st commit

--- src/cnnClassifier/constants/__init__.py
+++ src/cnnClassifier/constants/__init__.py
@@ -1,12 +1,16 @@
 import os
+from pathlib import Path
 
 # Current working directory
 current_dir = os.getcwd()
 
 # Relative paths
-CONFIG_FILE_PATH_RELATIVE = "../../../config/config.yaml"
-PARAMS_FILE_PATH_RELATIVE = "../../../params.yaml"
+CONFIG_FILE_PATH_RELATIVE = "config/config.yaml"
+PARAMS_FILE_PATH_RELATIVE = "params.yaml"
 
 # Convert to absolute paths
 CONFIG_FILE_PATH_ABSOLUTE = os.path.abspath(os.path.join(current_dir, CONFIG_FILE_PATH_RELATIVE))
+CONFIG_FILE_PATH_ABSOLUTE1 =Path(CONFIG_FILE_PATH_ABSOLUTE)
 PARAMS_FILE_PATH_ABSOLUTE = os.path.abspath(os.path.join(current_dir, PARAMS_FILE_PATH_RELATIVE))
+PARAMS_FILE_PATH_ABSOLUTE1 =Path(PARAMS_FILE_PATH_ABSOLUTE)
+print(CONFIG_FILE_PATH_RELATIVE)
